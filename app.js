63034f3132bee43c8c411a6feb37933c8ce304af
jantapabh <jantapa2407@gmail.com>
2020-02-21 23:51:07 +0700

    commit

--- app.js
+++ app.js
@@ -11,8 +11,11 @@ var express = require('express');
 var app = express();
 
 app.get('/greeting', (req, res) => {
-   let greetText = req.query.str1 + " " + req.query.str2
-   res.send(`<html><h1 style="align:center;">${greetText}</h1></body></html>`)
+
+
+    let greetText = req.query.str1 + " " + req.query.str2
+    res.send(`<html><h1 style="align:center;">${greetText}</h1></body></html>`)  //การส่งค่าขึ้น srever
+    
 })
 
 app.listen(8000);
