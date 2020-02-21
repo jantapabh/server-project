a52cb75c03f2fe4ef039db5eecf91070a58a3129
jantapabh <jantapa2407@gmail.com>
2020-02-21 23:49:59 +0700

    test commit

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+// var express = require('express');
+// var app = express();
+
+// app.get('/', function(req, res){
+//    res.send('Hello world')
+// });
+
+// app.listen(8000);
+
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-   res.send('Hello world')
-});
+app.get('/greeting', (req, res) => {
+   let greetText = req.query.str1 + " " + req.query.str2
+   res.send(`<html><h1 style="align:center;">${greetText}</h1></body></html>`)
+})
 
 app.listen(8000);
