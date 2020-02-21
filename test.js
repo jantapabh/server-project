3aab08bf4b6bd2db04c6f5e51306dab6e2c5a1be
jantapabh <jantapa2407@gmail.com>
2020-02-22 00:09:35 +0700

    test

--- test.js
+++ test.js
@@ -1,14 +1,24 @@
 var express = require('express')
 var app = express()
-var cookieParser = require('cookie-parser')
-app.use(cookieParser('keyboard cat')) //‘keyboard cat’ is a secret key to sign cookie (prevent cookie tamper)
-app.get('/ck_get', function (req, res) {
-    res.send(req.cookies)
-})
+var session = require('express-session')
 
-app.get('/ck_set', function (req, res) {
-    res.cookie('a', 10)
-    res.send('ok')
-})
+                         // sign cookie (for a session)
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 },
+   resave : false, saveUninitialized: false }))
+   // resave => Forces the session to be saved back to the session store, even if the session was never modified 
+   // saveUninitialized => the cookie will not be set on a response with an uninitialized session
 
+app.use(function(req, res, next) {
+   var sess = req.session
+   if (sess.views) {
+       sess.views++
+   } else {
+       sess.views = 1
+   }
+   console.log(sess.views)
+   next();
+})
+app.get('/',function(req, res) {
+   res.send('count =' + req.session.views)
+})
 app.listen(8000)
