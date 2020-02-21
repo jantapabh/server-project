434ce562ba40e276cb2677d908aa70a735ca19a5
jantapabh <jantapa2407@gmail.com>
2020-02-21 23:47:18 +0700

    test to express server

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
-const express = require('express');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-// This will be our application entry. We'll setup our server here.
-const http = require('http');
-// Set up the express app
-const app = express();
-// Log requests to the console.
-app.use(logger('dev'));
-// Parse incoming requests data (https://github.com/expressjs/body-parser)
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-// Setup a default catch-all route that sends back a welcome message in JSON format.
-app.get('*', (req, res) => res.status(200).send({
-    message: 'Welcome to the beginning of nothingness.',
-}));
-const port = parseInt(process.env.PORT, 10) || 8000;
-app.set('port', port);
-const server = http.createServer(app);
-server.listen(port);
-module.exports = app;
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+   res.send('Hello world')
+});
+
+app.listen(8000);
