47a01a516b0ef3b181369bc0fb89149b198d18ad
jantapabh <jantapa2407@gmail.com>
2020-02-21 23:55:36 +0700

    commit Params (req.params)

--- app.js
+++ app.js
@@ -7,15 +7,28 @@
 
 // app.listen(8000);
 
+// var express = require('express');
+// var app = express();
+
+
+// app.get('/greeting', (req, res) => {
+
+//     let greetText = req.query.str1 + " " + req.query.str2
+//     res.send(`<html><h1 style="align:center;">${greetText}</h1></body></html>`)  //การส่งค่าขึ้น srever
+
+// })
+
+// app.listen(8000);
+
+
 var express = require('express');
 var app = express();
 
-
-app.get('/greeting', (req, res) => {
-
-    let greetText = req.query.str1 + " " + req.query.str2
-    res.send(`<html><h1 style="align:center;">${greetText}</h1></body></html>`)  //การส่งค่าขึ้น srever
+app.get('/greeting/:str1/:str2', (req, res) => {
     
+    console.log(req)
+    let greetText = req.params.str1 + " " + req.params.str2
+    res.send(`<html><h1 style="align:center;">Hey: ${greetText}</h1></body></html>`)
 })
 
 app.listen(8000);
