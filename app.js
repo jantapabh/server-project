4392997f19a039f77d057746921b5d8fbf16f065
jantapabh <jantapa2407@gmail.com>
2020-02-21 23:58:25 +0700

    commit params

--- app.js
+++ app.js
@@ -20,12 +20,15 @@
 
 // app.listen(8000);
 
+// Params (req.params) 
+
+
 
 var express = require('express');
 var app = express();
 
 app.get('/greeting/:str1/:str2', (req, res) => {
-    
+
     console.log(req)
     let greetText = req.params.str1 + " " + req.params.str2
     res.send(`<html><h1 style="align:center;">Hey: ${greetText}</h1></body></html>`)
