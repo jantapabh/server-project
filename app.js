f2a549df4e9662db57cb4619fb6f4e145ca292d3
jantapabh <jantapa2407@gmail.com>
2020-02-22 00:01:02 +0700

    commit body pasre

--- app.js
+++ app.js
@@ -30,8 +30,10 @@ app.get('/greeting/:str1/:str2', (req, res) => {
     // console.log(req)
     let greetText = req.params.str1 + " " + req.params.str2
     res.send(`<html><h1 style="align:center;">Hey: ${greetText}</h1></body></html>`)
-    
+
 
 })
 
 app.listen(8000);
+
+
