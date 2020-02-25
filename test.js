b225d23edf03925883064f5299b82f002e579860
jantapabh <jantapa2407@gmail.com>
2020-02-25 13:44:12 +0700

    test commit

--- test.js
+++ test.js
@@ -30,9 +30,9 @@ var app = express();
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
+
 app.get('/fruit', function (req, res) {
     res.render('fruit', { fruits: ['banana', 'apple'], foo: 'bar' })
 })
-
 
 app.listen(8000)
